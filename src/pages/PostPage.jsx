1fd14123b3f37ec61a44e4cd28579048875bb3a5
sjwhole <bluemoon2718@naver.com>
2021-07-23 15:17:45 +0900

    feat: Add Link to my post in post page

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Redirect } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
 import { createPost } from "../utils/apis/Post";
 
 function PostPage() {
@@ -15,27 +15,32 @@ function PostPage() {
   }
 
   return localStorage.getItem("JWT") ? (
-    <div>
-      <form>
-        <input
-          type="number"
-          name="grade"
-          placeholder="오늘의 점수"
-          value={input.grade}
-          onChange={handleOnChange}
-        />
-        <input
-          type="text"
-          name="body"
-          placeholder="오늘의 이야기"
-          value={input.body}
-          onChange={handleOnChange}
-        />
-        <button type="submit" onClick={handleSubmit}>
-          Post
-        </button>
-      </form>
-    </div>
+    <>
+      <div>
+        <form>
+          <input
+            type="number"
+            name="grade"
+            placeholder="오늘의 점수"
+            value={input.grade}
+            onChange={handleOnChange}
+          />
+          <input
+            type="text"
+            name="body"
+            placeholder="오늘의 이야기"
+            value={input.body}
+            onChange={handleOnChange}
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Post
+          </button>
+        </form>
+      </div>
+      <div>
+        <Link to="/my">나의 이야기들 보러가기</Link>
+      </div>
+    </>
   ) : (
     <Redirect to="/login" />
   );
