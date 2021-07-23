87a2df21c13ccce663ca653610e7ae9fa51caca6
= sjwhole <bluemoon2718@naver.com>
2021-07-24 02:29:32 +0900

    feat: Add home page style

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,33 +1,53 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 function Homepage() {
   return (
-    <>
+    <LoginBlock>
       <section>
-        <span>우리의 이야기</span>
+        <span id="title">우리의 이야기</span>
       </section>
       <section>
-        <div>
-          {localStorage.getItem("JWT") ? (
-            <>
-              <Link to="/post">로그인 완료! Post로 이동</Link>
-              <button
-                onClick={() => {
-                  localStorage.removeItem("JWT");
-                  window.location.reload(false);
-                }}
-              >
-                Logout
-              </button>
-            </>
-          ) : (
-            <Link to="/login">Login</Link>
-          )}
-        </div>
+        {localStorage.getItem("JWT") ? (
+          <div id="logout">
+            <Link to="/post">로그인 완료! Post로 이동</Link>
+            <button
+              onClick={() => {
+                localStorage.removeItem("JWT");
+                window.location.reload(false);
+              }}
+            >
+              Logout
+            </button>
+          </div>
+        ) : (
+          <Link to="/login">Login</Link>
+        )}
       </section>
-    </>
+    </LoginBlock>
   );
 }
+
+const LoginBlock = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  background-color: beige;
+  height: 100vh;
+  #title {
+    font-size: 30px;
+  }
+  #logout {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    button {
+      margin-top: 15px;
+    }
+  }
+`;
 
 export default Homepage;
