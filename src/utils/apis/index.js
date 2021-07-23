3b5b6b20788e90896a226b467f8ef46044285ecf
sjwhole <bluemoon2718@naver.com>
2021-07-23 20:19:21 +0900

    feat: Separate axios auth case

--- src/utils/apis/index.js
+++ src/utils/apis/index.js
@@ -1,6 +1,14 @@
 import axios from "axios";
 
-export default () => {
+export const http = () => {
+  const Token = localStorage.getItem("JWT");
+  const request = axios.create({
+    baseURL: "http://3.34.142.4",
+  });
+  return request;
+};
+
+export const httpAuth = () => {
   const Token = localStorage.getItem("JWT");
   const request = axios.create({
     baseURL: "http://3.34.142.4",
