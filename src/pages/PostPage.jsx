30b1d88e0315aba1115a06b1f89182458e58ef53
sjwhole <bluemoon2718@naver.com>
2021-07-24 22:48:54 +0900

    feat: Add post page style button

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -17,28 +17,30 @@ function PostPage() {
 
   return localStorage.getItem("JWT") ? (
     <CreatePostBlock>
+      <h1>나의 하루 </h1>
       <div>
-        <form>
-          <input
-            type="number"
-            name="grade"
-            placeholder="오늘의 점수"
-            value={input.grade}
-            onChange={handleOnChange}
-          />
-          <input
-            type="text"
-            name="body"
-            placeholder="오늘의 이야기"
-            value={input.body}
-            onChange={handleOnChange}
-          />
-          <button type="submit" onClick={handleSubmit}>
-            Post
-          </button>
-        </form>
+        <input
+          type="number"
+          name="grade"
+          placeholder="오늘의 점수 1~5"
+          value={input.grade}
+          onChange={handleOnChange}
+        />
+        <textarea
+          type="text"
+          name="body"
+          placeholder="오늘의 이야기"
+          value={input.body}
+          onChange={handleOnChange}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Post
+        </button>
       </div>
-      <Link to="/my">나의 이야기들 보러가기</Link>
+      <div id="my">
+        <Link to="/my">나의 이야기들 보러가기</Link>
+        <Link to="/">홈으로</Link>
+      </div>
     </CreatePostBlock>
   ) : (
     <Redirect to="/login" />
@@ -51,8 +53,72 @@ const CreatePostBlock = styled.div`
   justify-content: center;
   align-items: center;
   height: 100vh;
-  & > a {
-    margin-top: 20px;
+  & > #my {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin: 1.5em 0;
+  }
+  a {
+    text-decoration: none;
+    color: black;
+    background: #71daec;
+    border: 0;
+    width: 100%;
+    padding: 1em;
+    margin: 1.5em 0;
+    box-sizing: border-box;
+    font-size: 14px;
+    text-align: center;
+  }
+  input {
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+    text-align: center;
+  }
+  textarea {
+    background: #f2f2f2;
+    width: 100%;
+    border: 0;
+    margin: 0 0 15px;
+    padding: 15px;
+    box-sizing: border-box;
+    font-size: 14px;
+    height: 200px;
+    resize: none;
+    ::-webkit-input-placeholder {
+      text-align: center;
+    }
+    :-moz-placeholder {
+      /* Firefox 18- */
+      text-align: center;
+    }
+    ::-moz-placeholder {
+      /* Firefox 19+ */
+      text-align: center;
+    }
+    :-ms-input-placeholder {
+      text-align: center;
+    }
+  }
+  button {
+    font-family: "Roboto", sans-serif;
+    text-transform: uppercase;
+    outline: 0;
+    background: #4caf50;
+    width: 100%;
+    border: 0;
+    padding: 15px;
+    color: #ffffff;
+    font-size: 14px;
+    -webkit-transition: all 0.3 ease;
+    transition: all 0.3 ease;
+    cursor: pointer;
   }
 `;
 
