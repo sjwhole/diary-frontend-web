62d92ac7e3922de500a3a27e5c14534299ba011f
sjwhole <bluemoon2718@naver.com>
2021-07-24 22:48:05 +0900

    feat: Add post item view style

--- src/components/PostItem/PostItem.jsx
+++ src/components/PostItem/PostItem.jsx
@@ -1,14 +1,27 @@
 import React from "react";
+import styled from "styled-components";
 
 function PostItem({ data }) {
   const { grade, body, created_at } = data;
   return (
-    <div>
-      <span>{grade}</span>
-      <span>{body}</span>
-      <span>{created_at}</span>
-    </div>
+    <PostBlock>
+      <span>나의 점수: {grade}점</span>
+      <br/>
+      <br/>
+      <span>나의 이야기: {body}</span>
+      <br/>
+      <br/>
+      <span>작성일: {created_at}</span>
+    </PostBlock>
   );
 }
+
+const PostBlock = styled.div`
+  background-color: #d5ffbd;
+  margin-bottom: 2em;
+  border-radius: 5%;
+  padding: 0.5em 0em;
+  width: 100%;
+`
 
 export default PostItem;
