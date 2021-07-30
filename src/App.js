5452d37126a442079db089dfd5b5b28c78840b38
sjwhole <bluemoon2718@naver.com>
2021-07-30 16:25:48 +0900

    feat: Add global style to render

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import GlobalStyle from "./styles/global";
 import Homepage from "./pages/Homepage";
 import LoginPage from "./pages/LoginPage";
 import PostPage from "./pages/PostPage";
@@ -8,15 +9,18 @@ import MyPostPage from "./pages/MyPostPage";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route exact path="/" component={Homepage} />
-        <Route exact path="/login" component={LoginPage} />
-        <Route exact path="/post" component={PostPage} />
-        <Route exact path="/kakao" component={KakaoLoginPage} />
-        <Route exact path="/my" component={MyPostPage} />
-      </Switch>
-    </Router>
+    <>
+      <GlobalStyle />
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route exact path="/login" component={LoginPage} />
+          <Route exact path="/post" component={PostPage} />
+          <Route exact path="/kakao" component={KakaoLoginPage} />
+          <Route exact path="/my" component={MyPostPage} />
+        </Switch>
+      </Router>
+    </>
   );
 };
 
